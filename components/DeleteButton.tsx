af937a2589623be73dc22b94161e976b7db0ece0
Arin7712 <aringawande7712@gmail.com>
2024-11-13 12:59:13 +0530

    update

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { deleteThread } from "@/lib/actions/thread.actions";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface DeleteButtonProps {
   threadId: string;
@@ -8,6 +9,15 @@ interface DeleteButtonProps {
 }
 
 const DeleteButton = ({ threadId, onAction}: DeleteButtonProps) => {
+
+  const [localTime, setLocalTime] = useState<string>("");
+
+
+  useEffect(() => {
+    // Client-side conversion to local time after the component mounts
+    const localTimeString = new Date("2024-11-13T07:20:36.671+00:00").toLocaleString(); // Adjusts to user's local time zone
+    setLocalTime(localTimeString);
+  }, ["2024-11-13T07:20:36.671+00:00"]);
 
   const router = useRouter();
   const handleDelete = async () => {
@@ -24,6 +34,7 @@ const DeleteButton = ({ threadId, onAction}: DeleteButtonProps) => {
   return (
     <button onClick={handleDelete} className="text-light-3">
       Delete
+      {localTime}
     </button>
   );
 };
